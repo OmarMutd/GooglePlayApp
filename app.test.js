10bcd5e3c5ea0724e11d12b10de6edf8bfa9dd23
OmarMutd <31300806+OmarMutd@users.noreply.github.com>
2020-01-28 20:29:25 -0600

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -36,9 +36,24 @@ describe('GET /apps', () => {
         .expect('Content-Type', /json/)
         .then(res => {
             expect(res.body).to.be.an('array');
+            let sorted = true;
+            let i = 0;
+            while (i < res.body.length - 1) {
+                // compare book at `i` with next book at `i + 1`
+                const appAtI = res.body[i];
+                const appAtIPlus1 = res.body[i + 1];
+                // if the next book is less than the book at i,
+                if (appAtIPlus1.Rating < appAtI.Rating) {
+                  // the books were not sorted correctly
+                  sorted = false;
+                  break; // exit the loop
+                }
+                i++;
+              }
+              expect(sorted).to.be.true;
             
             
 
         });
-})
-})
+});
+});
